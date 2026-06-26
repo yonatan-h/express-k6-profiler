898ae0e11964dd003ac14c97fcd003e929241c03
yonatan-h <yon019hl@gmail.com>
2026-06-26 22:13:25 +0300

    make things not too wide

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -11,18 +11,20 @@ export default function App() {
   return (
     <GlobalContextProvider>
       <ToastContainer />
-      <div className="bg-gray-100 w-full h-screen p-3 flex flex-col gap-3 text-gray-900">
-        <StatusBar />
-        <TopSummary />
+      <div className="bg-gray-100 w-full h-screen text-gray-900 overflow-hidden">
+        <div className="max-w-[1400px] mx-auto w-full h-full p-3 flex flex-col gap-3">
+          <StatusBar />
+          <TopSummary />
 
-        <div className="flex-1 flex gap-3 min-h-0">
-          <div className="flex-3 flex flex-col gap-3 min-h-0">
-            <Results />
+          <div className="flex-1 flex gap-3 min-h-0">
+            <div className="flex-3 flex flex-col gap-3 min-h-0">
+              <Results />
+            </div>
+
+            <Details />
           </div>
-
-          <Details />
+          <DebugLogsPopup />
         </div>
-        <DebugLogsPopup />
       </div>
     </GlobalContextProvider>
   );
@@ -39,31 +41,3 @@ function Metric({ label, value, change }: { label: string; value: string; change
   );
 }
 
-function Separator() {
-  return <div className="h-full min-h-4 w-px bg-gray-200" />;
-}
-
-function BaseLine() {
-  return (
-    <div className="flex gap-2 items-center">
-      <span className="text-xs">Compare:</span>
-      <select className="border border-gray-300 text-xs p-1 rounded">
-        <option>Baseline 1 (saved 2h ago)</option>
-        <option>None (Raw stats only)</option>
-      </select>
-
-      <div className="w-0.5 border border-gray-200 h-full" />
-      <button className="text-xs bg-gray-700 text-white py-2 px-3 rounded">Save</button>
-    </div>
-  );
-}
-
-
-
-function Nav() {
-  return (
-    <div className="p-2 w-full bg-white flex justify-between rounded  items-center border border-gray-200">
-      <div></div>
-    </div>
-  );
-}
